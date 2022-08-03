5ee99fe95a9ec668eafe7502ca5a1e48ba83e23c
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-08-03 19:30:23 -0400

    Added computation functions, debugged HTML
    Added addition, subtraction, division, multiplication, the functions of all-clear and deletion, and the appending the computational signs.
    
    Fixed HTML "data-al-clear" to "data-all-clear".

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ class Calculator {
     };
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
     };
 
     appendNumber(number) {
@@ -22,15 +22,50 @@ class Calculator {
     };
 
     chooseOperation(operation) {
-        
+        if (this.currentOperand === '') return;
+        if (this.previousOperand != '') {
+            this.compute();
+        };
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
     };
 
     compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '*':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            default:
+                return;
+        };
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    };
 
+    getDisplayNumber(number) {
+        return number
     };
 
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand;
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+        };
     };
 
 };
@@ -57,4 +92,19 @@ operationButtons.forEach(button => {
         calculator.chooseOperation(button.innerText);
         calculator.updateDisplay();
     });
+});
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
 });
